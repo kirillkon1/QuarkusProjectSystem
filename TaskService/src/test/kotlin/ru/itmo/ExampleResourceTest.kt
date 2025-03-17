67b99d89bfa9d5cbe0ed772ce7bce00b819bc64d
kirillkon1 <kirill.kondrashow@mail.ru>
2025-03-17 22:21:46 +0300

    Added .gitignore

--- TaskService/src/test/kotlin/ru/itmo/ExampleResourceTest.kt
+++ TaskService/src/test/kotlin/ru/itmo/ExampleResourceTest.kt
@@ -6,7 +6,7 @@ import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
-class ExampleResourceTest {
+open class ExampleResourceTest {
     @Test
     fun testHelloEndpoint() {
         given()
